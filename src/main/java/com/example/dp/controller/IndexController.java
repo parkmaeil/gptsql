edba2cd3d4100a9957cbc43b43887c650ed6affe
parkmaeil <bitcocom@empas.com>
2024-12-25 13:22:53 +0900

    gptsql

--- src/main/java/com/example/dp/controller/IndexController.java
+++ src/main/java/com/example/dp/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping("/sql")
     public String index(){
          return "sql";
     }
